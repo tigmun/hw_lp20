21fb60562588a81fd256e259a45231061e5b54c8
versedmm <versedmm@gmail.com>
2021-02-16 20:10:05 +0300

    add for_scores.py

---
+++ hw_week_1/for_scores.py
@@ -0,0 +1,12 @@
+#Создать список из десяти целых чисел.
+#Вывести на экран каждое число, увеличенное на 1.
+
+for i in range (10):
+	i += 1
+	print (i)
+
+
+input_word = str(input('Введите текст: '))
+word = input_word
+for letter in word:
+	print (letter)
